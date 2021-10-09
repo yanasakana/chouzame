a0ad369fcd1db94afeaeba4ed9b566b607e7ee49
yanasakana <yanagiryoichi@gmail.com>
2021-10-08 22:28:21 -0700

    Customised Mod Properties and Packages

--- src/main/java/com/yanasakana/chouzame/mixin/ChouzameMixin.java
+++ src/main/java/com/yanasakana/chouzame/mixin/ChouzameMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package com.yanasakana.chouzame.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import com.yanasakana.chouzame.Chouzame;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class ChouzameMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		Chouzame.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
